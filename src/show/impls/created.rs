7d61f41ac564aff0e7ee910a42b83227c95d830e
Cyril Plisko <cyril.plisko@mountall.com>
2025-08-24 13:16:52 +0300

    Refactor impl Show for Created

--- src/show/impls/created.rs
+++ src/show/impls/created.rs
@@ -4,32 +4,30 @@ use super::*;
 
 impl<K> Show for Created<K>
 where
-    K: ResourceExt,
+    K: Clone + ResourceExt + serde::Serialize,
     K::DynamicType: Default,
 {
     fn header(&self, _output: &OutputFormat) -> Vec<String> {
-        todo!()
+        vec![]
     }
 
     fn data(&self, _params: &ShowParams, _output: &OutputFormat) -> Vec<String> {
-        todo!()
+        let kind = K::kind(&default()).to_lowercase();
+        let name = self.k.name_any();
+        vec![format!("{kind}/{name} created")]
     }
 
-    fn json(&self, _params: &ShowParams) -> String {
-        todo!()
+    fn json(&self, params: &ShowParams) -> String {
+        let data = self.k.maybe_strip_managed_fields(params);
+        json::to_string_pretty(&data).unwrap_or_default()
     }
 
-    fn yaml(&self, _params: &ShowParams) -> String {
-        todo!()
+    fn yaml(&self, params: &ShowParams) -> String {
+        let data = self.k.maybe_strip_managed_fields(params);
+        yaml::to_string(&data).unwrap_or_default()
     }
 
     fn name(&self) -> String {
         todo!()
-    }
-
-    fn output(&self, _namespace: bool, _params: &ShowParams, _output: &OutputFormat) -> String {
-        let kind = K::kind(&default()).to_lowercase();
-        let name = self.k.name_any();
-        format!("{kind}/{name} created")
     }
 }

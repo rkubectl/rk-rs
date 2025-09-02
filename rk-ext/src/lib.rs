218a1ff07f7b9f1549f9017ff355c64daedaafd6
Cyril Plisko <cyril.plisko@mountall.com>
2025-09-02 14:35:31 +0300

    more ext traits moved to rk-ext

--- rk-ext/src/lib.rs
+++ rk-ext/src/lib.rs
@@ -3,21 +3,19 @@ use kube::api;
 use kube::core::gvk;
 use kube::discovery;
 
-// use kube_client_ext::KubeClientExt;
-// use serde_json as json;
-// use serde_yaml as yaml;
-// use tracing::debug;
-// use tracing::error;
-// use tracing::info;
-// use tracing::trace;
+use k8s::ContainerGetExt;
+use k8s::ContainerStatusGetExt;
+use k8s::NamespaceGetExt;
+use k8s::PodGetExt;
 
-// use k8s::authenticationv1;
-// use k8s::authorizationv1;
-// use k8s::corev1;
+use k8s::corev1;
 use k8s::metav1;
-// use k8s::rbacv1;
 
 pub use apiresource::APIResourceExt;
 pub use apiresource::APIResourceListExt;
+pub use namespace::NamespaceGetExt2;
+pub use pod::PodGetExt2;
 
 mod apiresource;
+mod namespace;
+mod pod;
